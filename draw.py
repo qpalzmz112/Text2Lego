d423da19a77af86fa9c97be219321ee42978f6ac
Sean Carlson <seancarlson112@gmail.com>
2023-04-20 19:24:26 -0500

    big progress

--- draw.py
+++ draw.py
@@ -1,9 +1,9 @@
 import variables as v
 
 if v.image_type == "png":
-	border_color = [174, 109, 226, 153] ## a nice lilac color
+	border = [174, 109, 226, 153] ## a nice lilac color
 elif v.image_type == "jpg":
-	border_color = [108, 68, 140]
+	border = [108, 68, 140]
 
 
 ## The draw functions take in a pixel coordinate from
@@ -12,6 +12,11 @@ elif v.image_type == "jpg":
 def start_brick(row, col, arr, color):
 	row *= 4
 	col *= 4
+	if v.color_mode == 1:
+		border_color = color
+	elif v.color_mode == 0:
+		border_color = border
+
 	for i in range(4):
 		arr[row][col+i] = border_color
 		arr[row+3][col+i] = border_color
@@ -23,30 +28,42 @@ def start_brick(row, col, arr, color):
 def continue_brick(row, col, arr, color):
 	row *= 4
 	col *= 4
+	if v.color_mode == 1:
+		border_color = color
+	elif v.color_mode == 0:
+		border_color = border
+
 	for i in range(4):
 		arr[row][col+i] = border_color
 		arr[row+3][col+i] = border_color
 		arr[row+1][col+i] = color
 		arr[row+2][col+i] = color
 
-def end_brick(row, col, arr, color, counter):
+def end_brick(row, col, arr, color, brick_length):
 	row *= 4
 	col *= 4
-	if counter == 1:
-		start = 1
-	else:
-		start = 0
-	for i in range(start, 4):
+	if v.color_mode == 1:
+		border_color = color
+	elif v.color_mode == 0:
+		border_color = border
+
+	for i in range(0, 4):
 		arr[row][col+i] = border_color
 		arr[row+3][col+i] = border_color
 		arr[row+i][col+3] = border_color
 		if i < 3:
-			arr[row+1][col+i] = color
-			arr[row+2][col+i] = color
+			if i != 0 or brick_length != 1:
+				arr[row+1][col+i] = color
+				arr[row+2][col+i] = color
 
-def draw_stud(row, col, arr):
+def draw_stud(row, col, arr, color=[-1]):
 	row *= 4
 	col *= 4
+	if color[0] != -1:
+		border_color = color
+	else:
+		border_color = border
+
 	arr[row+3][col+1] = border_color
 	arr[row+3][col+2] = border_color
 
@@ -58,3 +75,10 @@ def not_background(arr):
 			return True
 	elif v.image_type == "jpg":
 		return arr[0] != 255 or arr[1] != 255 or arr[2] != 255 ## return true if color is not pure white
+
+def same_color(color1, color2):
+	for (num1, num2) in zip(color1, color2):
+		if num1 != num2:
+			return False
+	return True
+
--- main.py
+++ main.py
@@ -3,10 +3,19 @@ import numpy as np
 from numpy import asarray
 import variables as v
 import draw as d
+import random
+
 
 in_img = Image.open(v.image_path)
-in_arr = asarray(in_img)
 num_cols, num_rows = in_img.size
+if num_cols > 50 or num_rows > 50:
+	x = max(num_cols, num_rows)
+	divisor = x // 50
+	in_img = in_img.resize((num_cols//divisor, num_rows//divisor))
+	num_cols, num_rows = in_img.size
+
+in_arr = asarray(in_img)
+
 
 if v.image_type == "png":
 	color_len = 4 ## rgba
@@ -15,57 +24,78 @@ elif v.image_type ==  "jpg":
 
 out_arr = np.full((4*num_rows, 4*num_cols, color_len), 255, dtype = np.uint8)
 
-max_len = max(v.brick_lengths)
+
 for row in range(num_rows):
-	counter = 0
-	for col in range(num_cols):
-		color = in_arr[row][col]
+	brick_length = 0
+	new_gaps = []
+	col = 0
+	while col < num_cols:
+		if v.color_mode == 0:
+			color = in_arr[row][col]
+		elif brick_length == 0:
+			if v.image_type == "png":
+				color = random.choice(v.lego_colors_rgba)
+			elif v.image_type == "jpg":
+				color = random.choice(v.lego_colors_rgb)
 
-		# if color and not started:
-		#	start brick, save start
-		# if started and color:
-		#	length += 1, continue brick
-		# if started and length = max length:
-		#	end brick
-		#   if next pixel is color:
-		#		store brick gap
-		# if started and not color:
-		#	if length is a brick length:
-		#		end brick
-		#	else:
-		#		end brick one back
+		## TODO: preserve color (end brick when color changes)
 
-		## TODO: store brick gaps, act on them, end brick when color changes, pixelate large images
-
-		if d.not_background(color) and not counter: ## pixel is colored and not currently drawing a brick
+		if d.not_background(in_arr[row][col]) and brick_length == 0: ## pixel is colored and not currently drawing a brick
 			d.start_brick(row, col, out_arr, color)
-			counter = 1
-			start_col = col
+			brick_length = 1
+			if v.color_mode == 0 and col < num_cols  - 1 and not d.same_color(color, in_arr[row][col+1]) and d.not_background(in_arr[row][col+1]):
+				d.end_brick(row, col, out_arr, color, brick_length)
 
-		elif d.not_background(color) and counter: ## pixel is colored and are currently drawing a brick
-			if counter + 1 == max_len: ## this is the final addition we can make to the current brick
-				# if gap, can't end here; need to make a shorter brick of possible length
-				d.end_brick(row, col, out_arr, color, counter)
-				counter = 0
+		elif d.not_background(in_arr[row][col]) and brick_length: ## pixel is colored and are currently drawing a brick
+			if brick_length + 1 == max(v.brick_lengths): ## this is the final addition we can make to the current brick
+				if col in previous_gaps: # if gap, can't end here; need to make a shorter brick
+					col -= 1
+					if v.color_mode == 1:
+						d.end_brick(row, col, out_arr, color, brick_length)
+					else:
+						d.end_brick(row, col, out_arr, in_arr[row][col], brick_length)
+					brick_length = 0
+				else:
+					d.end_brick(row, col, out_arr, color, brick_length)
+					brick_length = 0
+
+				if col < num_cols - 1:
+					if d.not_background(in_arr[row][col+1]):
+						new_gaps.append(col) ## gap is to the right of the stored index
+
+			elif v.color_mode == 0 and col < num_cols - 1 and not d.same_color(color, in_arr[row][col+1]) and d.not_background(in_arr[row][col+1]):
+				d.end_brick(row, col, out_arr, color, brick_length+1)
+				brick_length = 0
+				new_gaps.append(col)
 
 			else: ## can keep extending brick
 				d.continue_brick(row, col, out_arr, color)
-				counter += 1
+				brick_length += 1
 
-		elif counter: ## pixel is not colored and are currently drawing a brick
-			d.end_brick(row, col-1, out_arr, in_arr[row][col-1], counter) # should make sure counter + 1 is a possible length, use color of last pixel in brick
-			counter = 0
+		elif brick_length: ## pixel is not colored and are currently drawing a brick
+			if v.color_mode == 1:
+				d.end_brick(row, col-1, out_arr, color, brick_length)
+			elif v.color_mode == 0:
+				d.end_brick(row, col-1, out_arr, in_arr[row][col-1], brick_length) # should make sure length + 1 is a possible length, use color of last pixel in brick
+			brick_length = 0
 
-		if counter and col == num_cols - 1:  ## drawing a brick and are at the rightmost column of the image
-			d.end_brick(row, col, out_arr, color, counter)
+		if brick_length and col == num_cols - 1:  ## drawing a brick and reach the rightmost column of the image
+			d.end_brick(row, col, out_arr, color, brick_length)
+		col += 1
+	previous_gaps = new_gaps
 
 
 for col in range(num_cols):
 	brick = False
 	for row in range(1, num_rows):
-		if d.not_background(in_arr[row][col]) and not brick:
-			d.draw_stud(row - 1, col, out_arr)
-			brick = True
+		if d.not_background(in_arr[row][col]):
+			if brick == False:
+				if v.color_mode == 1:
+					d.draw_stud(row - 1, col, out_arr, out_arr[row*4][col*4])
+				elif v.color_mode == 0:
+					d.draw_stud(row - 1, col, out_arr)
+				if not v.studs_on_all_bricks:
+					brick = True
 		else:
 			brick = False
 
